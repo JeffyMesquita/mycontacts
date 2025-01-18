8527e3a732f890803fa29918aa04d50702b2f177
Jeferson Antonio Mesquita <je_2742@hotmail.com>
2025-01-18 18:08:51 -0300

    chore: Criando a rota de cadastro de Contatos

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -22,8 +22,30 @@ class ContactController {
     return response.json(contact);
   }
 
-  store() {
+  async store(request, response) {
     // Create a new contact
+    const { name, email, phone, category_id } = request.body;
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+
+    const contactAlreadyExists = await ContactsRepository.findByEmail(email);
+
+    if (contactAlreadyExists) {
+      return response
+        .status(400)
+        .json({ error: 'This email is already being used' });
+    }
+
+    const contact = await ContactsRepository.create({
+      name,
+      email,
+      phone,
+      category_id,
+    });
+
+    return response.json(contact);
   }
 
   update() {
